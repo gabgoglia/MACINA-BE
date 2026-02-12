c92ad1e82af240a262a3a4065214eeb3a5fb854e
Gabriele Goglia <gab.goglia@gmail.com>
2026-02-12 17:28:40 +0100

    project inizialized

---
+++ src/main/java/it/credem/macina_be/MacinaBeApplication.java
@@ -0,0 +1,13 @@
+package it.credem.macina_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacinaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacinaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/credem/macina_be/MacinaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.credem.macina_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacinaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
