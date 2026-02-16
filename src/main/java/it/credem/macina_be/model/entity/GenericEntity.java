c123d9b8fb7aec63e4505b9b99be667c8f026fbe
Lorenzo De Luise <72943042+ldeluise@users.noreply.github.com>
2026-02-16 11:35:45 +0100

    Aggiunto lombok dove manca

--- src/main/java/it/credem/macina_be/model/entity/GenericEntity.java
+++ src/main/java/it/credem/macina_be/model/entity/GenericEntity.java
@@ -1,4 +1,9 @@
 package it.credem.macina_be.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class GenericEntity {
 }
--- src/main/java/it/credem/macina_be/model/entity/Person.java
+++ src/main/java/it/credem/macina_be/model/entity/Person.java
@@ -1,29 +1,13 @@
 package it.credem.macina_be.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+@Data
+@AllArgsConstructor
 public class Person {
     private String firstName;
     private String lastName;
 
-    public Person() {}
-
-    public Person(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
