c348307ce6d6cd1aa67700dcdf4b6fadff3ba6df
paolovisca86 <paolovisca86@googlemail.com>
2026-02-17 09:55:27 +0100

    added log4J config

--- src/main/java/it/credem/macina_be/temporal/activities/impl/HelloActivitiesImpl.java
+++ src/main/java/it/credem/macina_be/temporal/activities/impl/HelloActivitiesImpl.java
@@ -1,12 +1,21 @@
 package it.credem.macina_be.temporal.activities.impl;
 
 import it.credem.macina_be.temporal.activities.HelloActivities;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 
 @Component
+@Log4j2
 public class HelloActivitiesImpl implements HelloActivities {
+
+    private static Integer attempt = 0;
     @Override
     public String composeGreeting(String name) {
-        return "Ciao " + name + " 👋";
+        attempt++;
+        if ((name.equalsIgnoreCase("err")) && (attempt < 3)) {
+            throw new RuntimeException("Simulated error");
+        }
+        log.info("[HelloActivities] executed - name={}", name);
+        return "Ciao " + "\"" + name + "\"" + " 👋";
     }
 }
