a710ae2430428f0c535bdd7fd3249f1bb39b3ec9
Lorenzo De Luise <72943042+ldeluise@users.noreply.github.com>
2026-02-19 12:25:08 +0100

    Added tests (not working at the moment)

--- src/test/java/it/credem/macina_be/controller/HelloWorldControllerTest.java
+++ src/test/java/it/credem/macina_be/controller/HelloWorldControllerTest.java
@@ -1,13 +1,16 @@
 package it.credem.macina_be.controller;
 
+import it.credem.macina_be.model.constant.MessageConstants;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,13 +20,23 @@ public class HelloWorldControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void whenHelloRequestThenReturns200() throws Exception {
+    public void whenHelloRequestThenReturnsCorrectPayloadGreetings1() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("api/hello")).andExpect(content().string("hello"));
+    }
+
+    @Test
+    public void whenHelloRequestThenReturnsCorrectPayloadGreetings2() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("api")).andExpect(content().string(MessageConstants.DEFAULT_HELLO_MESSAGE));
     }
 
 
     @Test
-    public void whenHelloRequestThenReturnsCorrectPayload() throws Exception {
+    public void whenHelloRequestThenReturnsCorrectPayloadGreetingsAndName() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("api/Mario" )).andExpect(content().string("Hello Mario!"));
+    }
+
+    @Test
+    public void whenHelloRequestPostThenReturns200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("api/hello").contentType(MediaType.APPLICATION_JSON_VALUE).content("Mario")).andExpect(status().isOk());
     }
 }
